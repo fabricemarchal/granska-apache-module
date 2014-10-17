7e6c37992f306769f481aa0f2bbe4ba9ffe16364
fabricemarchal <fabrice@invigos.com>
2014-10-17 20:38:20 +0000

    test

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ granska-apache-module
 Apache 2.4 module and wrapper around the KTH Granska library
 
 See https://github.com/viggokann/granska
+
+
--- test.sh
+++ test.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+curl http://localhost/ping.granska
 curl  -X POST -H "Content-Type: application/x-www-form-urlencoded;charset=ISO-8859-1" --data-binary @sample.txt http://localhost/run.granska
+curl http://localhost/stats.granska
