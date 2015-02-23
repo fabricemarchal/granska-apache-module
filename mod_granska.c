f363de854f4136e5ccf517d3db08bebec20540cd
fabrice marchal <fabrice@invigos.com>
2015-02-23 13:59:48 +0100

    set env variables

--- mod_granska.c
+++ mod_granska.c
@@ -99,6 +99,15 @@ static void granska_register_hooks(apr_pool_t *pool)
 	ap_log_perror(APLOG_MARK, APLOG_WARNING, APR_SUCCESS, pool, "registering module");
 	/* Hook the request handler */
 	ap_hook_handler(granska_handler, NULL, NULL, APR_HOOK_LAST);
+	
+	setenv("GRANSKA_HOME", ap_resolve_env("${GRANSKA_HOME}"), 1 );
+	setenv("STAVA_LEXICON", ap_resolve_env("${STAVA_LEXICON}"), 1 );
+	setenv("TAGGER_LEXICON", ap_resolve_env("${TAGGER_LEXICON}"), 1 );
+	setenv("SCRUTINIZER_RULE_FILE", ap_resolve_env("${SCRUTINIZER_RULE_FILE}"), 1 );
+	setenv("SCRUTINIZER_TEST_TEXT", ap_resolve_env("${SCRUTINIZER_TEST_TEXT}"), 1 );
+	setenv("DEVELOPERS_TAGGER_LEXICON", ap_resolve_env("${DEVELOPERS_TAGGER_LEXICON}"), 1 );
+	setenv("DEVELOPERS_TAGGER_OPT_TEXT", ap_resolve_env("${DEVELOPERS_TAGGER_OPT_TEXT}"), 1 );	
+		
 }
 
 
